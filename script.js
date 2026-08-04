eecc2c14130e2a5a218822b45c6b217120f8ff12
Chris <chrisgorham555@gmail.com>
2026-08-04 16:48:42 -0400

    added javascript interactivity, buttons, and more

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("Scruot is connected!");
+
+const toggleBtn1 = document.querySelector('#mathBtn');
+const contentBox1 = document.querySelector('#math');
+
+toggleBtn1.addEventListener('click', function() {
+  contentBox1.classList.toggle('isVisible');
+});
+
+const toggleBtn2 = document.querySelector('#javascriptbutton');
+const contentBox2 = document.querySelector('#javascript');
+
+toggleBtn2.addEventListener('click', function() {
+  contentBox2.classList.toggle('isVisible');
+});
